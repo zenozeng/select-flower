edeabef1c7fde063515780712e2b8b5a48c4d4ce
Zeno Zeng <zenoofzeng@gmail.com>
2015-06-22 15:47:59 +0800

    init op

---
+++ main.js
@@ -0,0 +1,27 @@
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+var target = document.getElementById('target');
+var source = document.getElementById('source');
+var ready = function(fn) {
+    if (target.width > 0 && source.width > 0) {
+        fn();
+    } else {
+        setTimeout(function() {
+            ready(fn);
+        }, 10);
+    }
+};
+
+ready(function() {
+    var w = target.width;
+    var h = target.height;
+    canvas.width = w;
+    canvas.height = h;
+
+    ctx.drawImage(target, 0, 0, w, h);
+    target = ctx.getImageData(0, 0, w, h);
+    ctx.drawImage(source, 0, 0, w, h);
+    source = ctx.getImageData(0, 0, w, h);
+
+    console.log(target, source);
+});
---
+++ op.js
@@ -0,0 +1,30 @@
+var createOp = function(fn) {
+    return function(imgdata1, imgdata2) {
+        var w = imgdata1.width;
+        var h = imgdata1.height;
+        var imgdata = new ImageData(w, h);
+        var d = imgdata.data;
+        var d1 = imgdata1.data;
+        var d2 = imgdata2.data;
+        for (var i = 0; i < d1.length; i++) {
+            d[i] = (i + 1) % 4 === 0 ? 255 : fn(d1[i], d2[i], i);
+        }
+        return imgdata;
+    };
+};
+
+var sub = createOp(function(v1, v2, i) {
+    return v1 - v2;
+});
+
+var add = createOp(function(v1, v2, i) {
+    return Math.min(v1 + v2, 255);
+});
+
+var mul = createOp(function(v1, v2) {
+    return Math.min(v1 * v2, 255);
+});
+
+var invert = createOp(function(v1) {
+    return 255 - v1;
+});
